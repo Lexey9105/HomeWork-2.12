8f6d866a6ace3c09017cc2c41ff45dcdf4d28273
Lexey9105 <umpp120@gmail.com>
2023-07-06 20:19:48 +0300

    Initial Commit

---
+++ src/main/java/pro/sky/Calculator_Sky/CalculatorSkyApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Calculator_Sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSkyApplication.class, args);
+	}
+
+}
